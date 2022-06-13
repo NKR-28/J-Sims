f7957e33afe49db9462d07f566deb36bffe9253f
Nita Roberts <91577650+NKR-28@users.noreply.github.com>
2022-06-13 12:54:06 -0700

    initial commit

---
+++ urban-winner/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const title = "Welcome!";
+  return (
+    <div className="App">
+          <div className="Content">
+            <h1>{title}</h1>
+          </div>
+        </div>
+  );
+}
+
+export default App;
